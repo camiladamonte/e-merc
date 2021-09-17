ae7f3bc85c2bdd6df13d9f7611698635426f1fb7
camiladamonte <camila292720@gmail.com>
2021-09-17 09:35:12 -0300

    json

--- js/product-info.js
+++ js/product-info.js
@@ -1,40 +1,12 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+const PROD1 = "https://raw.githubusercontent.com/camiladamonte/e-merc/main/JSON/Chevrolet_Onix_Joy.json";
+const PROD2 = "https://raw.githubusercontent.com/camiladamonte/e-merc/main/JSON/Fiat_Way.json";
+const PROD3 = "https://raw.githubusercontent.com/camiladamonte/e-merc/main/JSON/Peugeot_208.json";
+const PROD4 = "https://raw.githubusercontent.com/camiladamonte/e-merc/main/JSON/Suzuki_Celerio.json";
+
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
 
-function MostrarPs (Prs){
-
-    let contenido="";
-        contenido += 'Nombre: ' + Prs.name + '<br>';
-        contenido += 'Descripción: ' + Prs.description + '<br>';
-        contenido += 'Precio: ' + Prs.cost + '<br>';
-        contenido += '<img src="img/prod'+i+'.jpg" alt="" height="90" widht="90"';
-        contenido += '<button onclick="redir('+i+')">Mas Info</button><br><br>';
-        contenido += 'Moneda: ' + Prs.currency + '<br>';
-        contenido += 'Vendidos: ' + Prs.SoldCount + '<br>';
-        contenido += 'Categoría: ' + Prs.category + '<br>';
-        for (let i = 0; i<5; i++){
-        contenido += '<img src="img/prod1_'+i+'.jpg" alt="" height="90" widht="90">';}
-    
-    document.getElementById("infor").innerHTML = contenido;
-}
-
-function noexisto() {
-    let contenido="";
-    contenido += 'Lo sentimos pero no tenemos más informacion de este producto por el momento.'
-}
-
-document.addEventListener("DOMContentLoaded", function (e){
-    GetJSONData(PRODUCT_INFO_URL).then(function(resultObj){
-        if(resultObj.status === "ok") {
-            if (JSON.parse(localStorage.getItem('info')).idprod == 0) {
-                MostrarPs(resultObj.data)
-            }else{
-                noexisto();
-            }
-        }
-    })
-});    
